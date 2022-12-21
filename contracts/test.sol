06ad8ffbc6425ffed25188a36284a0fae4999d34
Bioneisme <bioneisme@gmail.com>
2022-12-21 13:40:25 +0600

    Updated test contract

---
+++ contracts/test.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract Inbox {
+    mapping(string => string) public ipfsInbox;
+    //Events
+    event ipfsSent(string _ipfsHash, string _address);
+    event inboxResponse(string response);
+    //Modifiers
+    modifier notFull (string memory _string) {
+        bytes memory stringTest = bytes(_string);
+        require(stringTest.length == 0);
+        _;
+    }
+    //takes in receiver's address and IPFS hash. Places the IPFSadress in the receiver's inbox
+    function sendIPFS(string memory _address, string memory _ipfsHash) public {
+        ipfsInbox[_address] = _ipfsHash;
+        emit ipfsSent(_ipfsHash, _address);
+    }
+    //retrieves hash
+    function getHash(string memory _address) public view returns (string memory) {
+        string memory ipfs_hash = ipfsInbox[_address];
+        //emit inboxResponse(ipfs_hash);
+        return ipfs_hash;
+    }
+}
