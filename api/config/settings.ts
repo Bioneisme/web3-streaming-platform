4cb95ccaeff7a96b9c0d2dbde136a5124ce34d0b
Bioneisme <bioneisme@gmail.com>
2022-12-21 13:42:11 +0600

    Added live stream translation

--- api/config/settings.ts
+++ api/config/settings.ts
@@ -7,3 +7,19 @@ export const DB_URI: string = process.env.DB_URI || 'postgres://postgres:root@po
 export const SERVER_PORT: number = +(process.env.SERVER_PORT || 5000);
 export const CLIENT_URL: string = process.env.CLIENT_URL || 'http://localhost:3000';
 export const JWT_SECRET: string = process.env.JWT_SECRET || "secretKey123123";
+export const INFURA_PROJECT_ID: string = process.env.INFURA_PROJECT_ID as string;
+export const INFURA_API_KEY: string = process.env.INFURA_API_KEY as string;
+export const STREAM_CONFIG = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 30,
+        ping_timeout: 60
+    },
+    http: {
+        port: 8000,
+        allow_origin: '*',
+        mediaroot: './media'
+    }
+};
--- api/server.ts
+++ api/server.ts
@@ -2,17 +2,31 @@ import express, {Application} from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import {EntityManager, MikroORM} from "@mikro-orm/core";
-import {CLIENT_URL, SERVER_PORT} from "./config/settings";
+import {CLIENT_URL, SERVER_PORT, STREAM_CONFIG} from "./config/settings";
 import {config} from "./config/mikro-orm";
 import logger from "./config/logger";
 import logging from "./middlewares/loggingMiddleware";
 import userRoute from "./routes/userRoute";
+import Web3 from "web3";
+// @ts-ignore
+import contract from "truffle-contract";
+import artifacts from "../build/Inbox.json";
+import NodeMediaServer from "node-media-server";
 
 const app: Application = express();
 
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const LMS = contract(artifacts);
+LMS.setProvider(web3.currentProvider);
+
+const nms = new NodeMediaServer(STREAM_CONFIG);
+nms.run();
+
 export const DI = {} as {
     orm: MikroORM,
-    em: EntityManager
+    em: EntityManager,
+    lms: any,
+    accounts: any
 };
 
 app.use(express.json());
@@ -28,6 +42,8 @@ app.use(logging);
 app.listen(SERVER_PORT, async () => {
     DI.orm = await MikroORM.init(config);
     DI.em = DI.orm.em.fork();
+    DI.lms = await LMS.deployed();
+    DI.accounts = await web3.eth.getAccounts();
 
     logger.info(`Server Started on port ${SERVER_PORT}`);
 });
