5b07fd38e46352360113acd3a92293659a2fc39f
Bioneisme <bioneisme@gmail.com>
2022-12-23 12:34:34 +0600

    Added bsctest and bsc networks, verify plugin

--- truffle-config.js
+++ truffle-config.js
@@ -3,31 +3,35 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const {MNEMONIC, ETH_PROJECT_ID} = process.env;
+const {MNEMONIC, BSC_SCAN_API_KEY, RPC} = process.env;
 
 module.exports = {
     contracts_build_directory: path.join(__dirname, "/build"),
+    api_keys: {
+        bscscan: BSC_SCAN_API_KEY
+    },
+    plugins: ['truffle-plugin-verify'],
     networks: {
         development: {
             host: "127.0.0.1",     // Localhost (default: none)
             port: 8545,            // Standard Ethereum port (default: none)
             network_id: "*",       // Any network (default: none)
         },
-        goerli: {
-            provider: () =>
-                new HDWalletProvider(
-                    {
-                        mnemonic: {
-                            phrase: MNEMONIC
-                        },
-                        providerOrUrl: ETH_PROJECT_ID,
-                        numberOfAddresses: 1
-                    }
-                ),
-            network_id: 5, // Goerli's id
-            confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
-            timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-            skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+        bsctest: {
+            networkCheckTimeout: 20000,
+            provider: () => new HDWalletProvider(MNEMONIC, RPC),
+            network_id: 97,
+            confirmations: 10,
+            timeoutBlocks: 200,
+            skipDryRun: true
+        },
+        bsc: {
+            networkCheckTimeout: 10000,
+            provider: () => new HDWalletProvider(MNEMONIC, `https://bsc-dataseed1.binance.org`),
+            network_id: 56,
+            confirmations: 10,
+            timeoutBlocks: 200,
+            skipDryRun: true
         },
     },
     mocha: {
