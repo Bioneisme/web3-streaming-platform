994931d92ef3ced5cc7e49eac161583defadb1cb
Bioneisme <bioneisme@gmail.com>
2022-12-24 14:58:01 +0600

    Updated contracts

--- contracts/soulboundToken.sol
+++ contracts/soulboundToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 import "../.deps/npm/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../.deps/npm/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
